277b985a0358450eab89990ab0aedf4bebc60401
Samet Oussema <sametoussema@gmail.com>
2016-02-17 11:28:37 +0100

    First commit

---
+++ js/angularHandler.js
@@ -0,0 +1,6 @@
+function angularHandler($scope, $http) {
+    $http.get('http://rest-service.guides.spring.io/greeting').
+        success(function(data) {
+            $scope.greeting = data;
+        });
+}
